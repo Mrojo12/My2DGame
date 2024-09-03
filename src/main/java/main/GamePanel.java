2be22788fbb39a81b102a1d3706de1f23babf6c7
Mrojo12 <mario.rojod@edu.uah.es>
2024-09-03 19:39:21 +0200

    Cuarto video:    1º Dibujo del mapa mediante un archivo .txt

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,14 +12,15 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48px x 48px
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768px
     final int screenHeight = tileSize * maxScreenRow; // 576px
 
     // FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyHandler = new KeyHandler();
     Thread gameThread; // Hilo del juego
     Player player = new Player(this, keyHandler);
@@ -67,6 +69,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D) g;
 
+        tileM.draw(g2);
         player.draw(g2);
 
         g2.dispose(); // Libera los recursos de memoria
