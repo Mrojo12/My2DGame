751e66d19c1360ed8a6b276e9f376897545fa462
Mrojo12 <mario.rojod@edu.uah.es>
2024-09-03 17:55:40 +0200

    Segundo video:    1º Introducción del movimiento del personaje    2º Introducción de los FPS (60)

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -14,12 +14,23 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768px
     final int screenHeight = tileSize * maxScreenRow; // 576px
 
+    // FPS
+    final int FPS = 60;
+
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread; // Hilo del juego
+
+    // SET PLAYER DEFAULT POSITION
+    int posX = 100;
+    int posY = 100;
+    int speed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); // Previene el parpadeo de la pantalla
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -29,6 +40,46 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
+        while (gameThread != null){
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+
+        }
+    }
+
+    public void update(){
+        if (keyHandler.upPressed){
+            posY -= speed;
+        } else if (keyHandler.downPressed){
+            posY += speed;
+        } else if (keyHandler.leftPressed){
+            posX -= speed;
+        } else if (keyHandler.rightPressed){
+            posX += speed;
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(posX,posY,tileSize,tileSize);
+
+        g2.dispose(); // Libera los recursos de memoria
     }
 }
