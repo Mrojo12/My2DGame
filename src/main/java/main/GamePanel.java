2878ceabd7fff3f0b14379a3c90a14a1312a9a77
Mrojo12 <mario.rojod@edu.uah.es>
2024-09-04 03:26:20 +0200

    Sexto video:    1º Introcucción de los objetos en el mapa, pero no tienen interactividad

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 import javax.swing.*;
@@ -30,7 +31,9 @@ public class GamePanel extends JPanel implements Runnable{
     KeyHandler keyHandler = new KeyHandler();
     Thread gameThread; // Hilo del juego
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
     public Player player = new Player(this, keyHandler);
+    public SuperObject[] obj = new SuperObject[10];
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -38,6 +41,10 @@ public class GamePanel extends JPanel implements Runnable{
         this.setDoubleBuffered(true); // Previene el parpadeo de la pantalla
         this.addKeyListener(keyHandler);
         this.setFocusable(true);
+    }
+
+    public void setupGame(){
+        aSetter.setObject();
     }
 
     public void startGameThread(){
@@ -76,7 +83,17 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D) g;
 
+        // TILE
         tileM.draw(g2);
+
+        // OBJECT
+        for (int i = 0; i < obj.length; i++){
+            if (obj[i] != null){
+                obj[i].draw(g2, this);
+            }
+        }
+
+        // PLAYER
         player.draw(g2);
 
         g2.dispose(); // Libera los recursos de memoria
