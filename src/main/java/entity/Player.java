401b2642dd18aea9d0f6bc6f986ea3f646e801c5
Mrojo12 <158284787+Mrojo12@users.noreply.github.com>
2024-09-06 20:27:35 +0200

    Décimo cuarto video
    Introducción de las imágenes del NPC_OldMan.
    Introducción del movimiento, colisiones (jugador-npc y npc-jugador) y movimiento aleatorio del NPC

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -9,16 +9,14 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Player extends Entity{
-    GamePanel gp;
     KeyHandler keyH;
-
     public final int screenX;
     public final int screenY;
 
-    // public int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH){
-        this.gp = gp;
+        super(gp);
+
         this.keyH = keyH;
 
         screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
@@ -45,29 +43,14 @@ public class Player extends Entity{
 
     public void getPlayerImage(){
 
-        up1 = setup("boy_up_1");
-        up2 = setup("boy_up_2");
-        down1 = setup("boy_down_1");
-        down2 = setup("boy_down_2");
-        left1 = setup("boy_left_1");
-        left2 = setup("boy_left_2");
-        right1 = setup("boy_right_1");
-        right2 = setup("boy_right_2");
-    }
-
-    public BufferedImage setup(String imageName){
-
-        UtilityTool uTool = new UtilityTool();
-        BufferedImage image = null;
-
-        try{
-            image = ImageIO.read(getClass().getResourceAsStream("/player/" + imageName + ".png"));
-            image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return image;
+        up1 = setup("/player/boy_up_1");
+        up2 = setup("/player/boy_up_2");
+        down1 = setup("/player/boy_down_1");
+        down2 = setup("/player/boy_down_2");
+        left1 = setup("/player/boy_left_1");
+        left2 = setup("/player/boy_left_2");
+        right1 = setup("/player/boy_right_1");
+        right2 = setup("/player/boy_right_2");
     }
 
     public void update(){
@@ -86,6 +69,10 @@ public class Player extends Entity{
             // CHECK TILE COLLISION
             collisionOn = false;
             gp.cChecker.checkTile(this);
+
+            // CHECK NPC COLLISION
+            int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
+            interacNPC(npcIndex);
 
             // CHECK OBJECT COLLISION
             int objIndex = gp.cChecker.checkObject(this, true);
@@ -124,6 +111,13 @@ public class Player extends Entity{
     public void pickUpObject(int index){
         if (index != 999){
 
+        }
+    }
+
+    public void interacNPC(int index){
+        if (index != 999){
+            System.out.println("You are hitting an NPC");
+            // gp.npc[index].setAction();
         }
     }
 
